cafe95e21ee0a9d30dfc67fb6eff694c473cedda
Mateusz Płonka <43822422+SmartMaatt@users.noreply.github.com>
2023-07-07 23:15:13 +0200

    Reading angle data from a c3d file
    * Added reading of group labels.
    * Added reading of frame count and parameters
    * Added reading of indexes of a given group based on the label.
    * Created construction of 3D table storing the change of euler angles in each angle and each frame.
    * Added saving of rotation change to csv file.

--- App/test.py
+++ App/test.py
@@ -1,36 +1,49 @@
-import numpy as np
 import ezc3d
+import csv
 
 def read_c3d_file(file_path):
-    # Otwórz plik C3D
     c3d = ezc3d.c3d(file_path)
 
-    # Wczytaj etykiety kątów
+    labels = c3d['parameters']['POINT']['LABELS']['value']
     angle_labels = c3d['parameters']['POINT']['ANGLES']['value']
+    angle_indecies = []
 
-    # Dla każdej etykiety, wypisz odpowiadające jej dane
-    for label in angle_labels:
-        angle_data = c3d['data']['points'][label]
-        print(f'Dane dla kąta {label}: {angle_data}')
+    for angle_label in angle_labels:
+        angle_indecies.append(labels.index(angle_label))
 
-    # # Pobierz listę nazw kanałów danych analogowych
-    # analog_labels = c3d['parameters']['POINT']['ANGLES']['value']
+    print(angle_indecies)
 
-    # # Sprawdź, czy istnieją jakiekolwiek dane analogowe
-    # if not analog_labels:
-    #     print(f"Brak danych analogowych w pliku {file_path}")
-    #     return None
+    # Frame count
+    header_points = c3d['header']['points']
+    first_frame = header_points['first_frame'] 
+    last_frame = header_points['last_frame']
+    frame_count = last_frame - first_frame + 1
 
-    # # Pobierz dane analogowe
-    # analog_data = c3d['data']['analogs']
+    data = c3d['data']['points']
+    angles_data = []
 
-    # # Zwróć dane analogowe jako słownik, gdzie klucze to nazwy kanałów, a wartości to dane z tych kanałów
-    # return {label: analog_data[i] for i, label in enumerate(analog_labels)}
+    for frame in range(frame_count):
+        angles_data.append([])
+        for index in angle_indecies:
+            angles_data[frame].append([data[0][index][frame], data[1][index][frame], data[2][index][frame]])
+
+    with open("C:/Users/mateu/Desktop/Obrobione/dupa.csv", 'w', newline='') as f:
+        writer = csv.writer(f)
+        frame_index = first_frame
+
+        for frame in angles_data:
+            angles = []
+            angles.append(frame_index)
+
+            for angle in frame:
+                angles.append(angle[0])
+                angles.append(angle[1])
+                angles.append(angle[2])
+            writer.writerow(angles)
+            frame_index += 1
+
+    
 
 
 file_path = "C:/Users/mateu/Desktop/Obrobione/Test406.c3d"
 read_c3d_file(file_path)
-
-# if data is not None:
-#     for label, values in data.items():
-#         print(f"Kanał: {label}, wartości: {values}")
